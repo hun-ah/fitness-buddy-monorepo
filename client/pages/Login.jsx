d13c933fd15bcd7c1cf444463c00e06966181b89
Hannah Machado <hannahmachado@yahoo.ca>
2024-01-31 11:07:51 -0500

    refactored inputs to use new component

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
-import { Button, Label, TextInput, Spinner } from 'flowbite-react';
+import { Button, Spinner } from 'flowbite-react';
 import { useNavigate } from 'react-router-dom';
 import { useAppContext as IsLoading } from '@/components/contexts/IsLoadingContext';
 import apiUrl from '@/api';
+import Input from '@/components/inputs/home/Input';
+import { loginInputs } from '@/props/props';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -103,65 +105,24 @@ const Login = () => {
     }
   };
 
+  // pass Login component state to loginInputs (prevent errors in props.js file) and store in new variable to map over
+  const inputs = loginInputs(formInputs, errMsg);
+
   return (
     <section className='bg-white dark:bg-gray-900 flex-1 items-center px-4'>
       <form
         className='flex max-w-md flex-col gap-4 mx-auto py-[130px]'
         onSubmit={() => handleFormSubmit(event)}
       >
-        <div>
-          <div className='mb-2 block'>
-            <Label
-              htmlFor='email'
-              value='Email'
-              color={errMsg.email && 'failure'}
-            />
-          </div>
-          <TextInput
-            id='email'
-            name='email'
-            type='text'
-            value={formInputs.email}
-            placeholder='name@fitnessbuddy.com'
-            shadow
-            color={errMsg.email ? 'failure' : 'gray'}
-            helperText={errMsg.email && errMsg.email}
-            className='textInput'
-            onChange={() => {
-              handleInputChange(event);
-              setErrMsg((prevMsg) => ({
-                ...prevMsg,
-                email: '',
-              }));
-            }}
+        {inputs.map((input) => (
+          <Input
+            key={input.label.htmlFor}
+            label={input.label}
+            input={input.input}
+            handleInputChange={handleInputChange}
+            setErrMsg={setErrMsg}
           />
-        </div>
-        <div>
-          <div className='mb-2 block'>
-            <Label
-              htmlFor='password'
-              value='Password'
-              color={errMsg.password && 'failure'}
-            />
-          </div>
-          <TextInput
-            id='password'
-            name='password'
-            type='password'
-            value={formInputs.password}
-            shadow
-            color={errMsg.password ? 'failure' : 'gray'}
-            helperText={errMsg.password && errMsg.password}
-            className='textInput'
-            onChange={() => {
-              handleInputChange(event);
-              setErrMsg((prevMsg) => ({
-                ...prevMsg,
-                password: '',
-              }));
-            }}
-          />
-        </div>
+        ))}
         {isLoading ? (
           <Button color='blue'>
             <Spinner aria-label='Spinner button example' size='sm' />
