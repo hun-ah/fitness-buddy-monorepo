8bd133b5813dc9552636572f6ba56d8e7ac2f0ad
Hannah Machado <hannahmachado@yahoo.ca>
2024-01-31 11:11:44 -0500

    changed function to explicit return

--- client/src/props/props.js
+++ client/src/props/props.js
@@ -1,29 +1,31 @@
-export const loginInputs = (formInputs, errMsg) => [
-  {
-    label: {
-      htmlFor: 'email',
-      labelValue: 'Email',
+export const loginInputs = (formInputs, errMsg) => {
+  return [
+    {
+      label: {
+        htmlFor: 'email',
+        labelValue: 'Email',
+      },
+      input: {
+        id: 'email',
+        inputValue: formInputs.email,
+        errMsg: errMsg.email,
+        name: 'email',
+        placeholder: 'name@fitnessbuddy.com',
+        type: 'text',
+      },
     },
-    input: {
-      id: 'email',
-      inputValue: formInputs.email,
-      errMsg: errMsg.email,
-      name: 'email',
-      placeholder: 'name@fitnessbuddy.com',
-      type: 'text',
+    {
+      label: {
+        htmlFor: 'password',
+        labelValue: 'Password',
+      },
+      input: {
+        id: 'password',
+        inputValue: formInputs.password,
+        errMsg: errMsg.password,
+        name: 'password',
+        type: 'password',
+      },
     },
-  },
-  {
-    label: {
-      htmlFor: 'password',
-      labelValue: 'Password',
-    },
-    input: {
-      id: 'password',
-      inputValue: formInputs.password,
-      errMsg: errMsg.password,
-      name: 'password',
-      type: 'password',
-    },
-  },
-];
+  ];
+};
