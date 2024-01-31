274d27e85b41c0431cccc343aedabed369068cc9
Hannah Machado <hannahmachado@yahoo.ca>
2024-01-31 11:29:57 -0500

    added registerInputs info

--- client/src/props/props.js
+++ client/src/props/props.js
@@ -29,3 +29,90 @@ export const loginInputs = (formInputs, errMsg) => {
     },
   ];
 };
+
+export const registerInputs = (formInputs, errMsg) => {
+  return [
+    {
+      label: {
+        htmlFor: 'firstName',
+        labelValue: 'First Name',
+      },
+      input: {
+        id: 'firstName',
+        inputValue: formInputs.firstName,
+        errMsg: errMsg.firstName,
+        name: 'firstName',
+        placeholder: 'Jane',
+        type: 'text',
+      },
+    },
+    {
+      label: {
+        htmlFor: 'lastName',
+        labelValue: 'Last Name',
+      },
+      input: {
+        id: 'lastName',
+        inputValue: formInputs.lastName,
+        errMsg: errMsg.lastName,
+        name: 'lastName',
+        placeholder: 'Doe',
+        type: 'text',
+      },
+    },
+    {
+      label: {
+        htmlFor: 'phone',
+        labelValue: 'Phone number',
+      },
+      input: {
+        id: 'phone',
+        inputValue: formInputs.phone,
+        errMsg: errMsg.phone,
+        name: 'phone',
+        placeholder: '647-123-4567',
+        type: 'text',
+      },
+    },
+    {
+      label: {
+        htmlFor: 'email',
+        labelValue: 'Email',
+      },
+      input: {
+        id: 'email',
+        inputValue: formInputs.email,
+        errMsg: errMsg.email,
+        name: 'email',
+        placeholder: 'name@fitnessbuddy.com',
+        type: 'text',
+      },
+    },
+    {
+      label: {
+        htmlFor: 'password',
+        labelValue: 'Password',
+      },
+      input: {
+        id: 'password',
+        inputValue: formInputs.password,
+        errMsg: errMsg.password,
+        name: 'password',
+        type: 'password',
+      },
+    },
+    {
+      label: {
+        htmlFor: 'confirm-password',
+        labelValue: 'Confirm password',
+      },
+      input: {
+        id: 'confirmPassword',
+        inputValue: formInputs.confirmPassword,
+        errMsg: errMsg.confirmPassword,
+        name: 'confirmPassword',
+        type: 'password',
+      },
+    },
+  ];
+};
