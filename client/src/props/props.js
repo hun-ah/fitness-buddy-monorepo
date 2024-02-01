40b78fae59a1928e27e1aeb4d1148648a3317825
Hannah Machado <hannahmachado@yahoo.ca>
2024-01-31 20:28:35 -0500

    added client profile input data

--- client/src/props/props.js
+++ client/src/props/props.js
@@ -1,4 +1,8 @@
-import { formatPhoneNumber } from '@/helpers/helpers';
+import {
+  formatPhoneNumber,
+  formatFirstName,
+  formatLastName,
+} from '@/helpers/helpers';
 
 export const loginInputs = (formInputs, errMsg) => {
   return [
@@ -203,6 +207,81 @@ export const profileInputs = (formInputs, errMsg, user) => {
         name: 'password',
         placeholder: '••••••••',
         type: 'password',
+      },
+    },
+  ];
+};
+
+export const clientProfileInputs = (formInputs, errMsg, client) => {
+  return [
+    {
+      label: {
+        htmlFor: 'firstName',
+        labelValue: 'First Name',
+      },
+      input: {
+        id: 'firstName',
+        inputValue: formInputs.firstName,
+        errMsg: errMsg.firstName,
+        name: 'firstName',
+        placeholder: formatFirstName(client.firstName),
+        type: 'text',
+      },
+    },
+    {
+      label: {
+        htmlFor: 'lastName',
+        labelValue: 'Last Name',
+      },
+      input: {
+        id: 'lastName',
+        inputValue: formInputs.lastName,
+        errMsg: errMsg.lastName,
+        name: 'lastName',
+        placeholder: formatLastName(client.lastName),
+        type: 'text',
+      },
+    },
+    {
+      label: {
+        htmlFor: 'email',
+        labelValue: 'Email',
+      },
+      input: {
+        id: 'email',
+        inputValue: formInputs.email,
+        errMsg: errMsg.email,
+        name: 'email',
+        placeholder: client.email,
+        type: 'text',
+      },
+    },
+    {
+      label: {
+        htmlFor: 'phone',
+        labelValue: 'Phone number',
+      },
+      input: {
+        id: 'phone',
+        inputValue: formInputs.phone,
+        errMsg: errMsg.phone,
+        name: 'phone',
+        placeholder: formatPhoneNumber(client.phone),
+        type: 'text',
+      },
+    },
+    {
+      label: {
+        htmlFor: 'sessions',
+        labelValue: 'Phone number',
+      },
+      input: {
+        id: 'sessions',
+        inputValue: formInputs.sessions,
+        errMsg: errMsg.sessions,
+        name: 'sessions',
+        placeholder: client.sessions,
+        type: 'text',
       },
     },
   ];
