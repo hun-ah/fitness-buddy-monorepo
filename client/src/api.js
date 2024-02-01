d4e4673221328ad42b0522f451b6d5a3c5f812f0
Hannah Machado <hannahmachado@yahoo.ca>
2024-02-01 10:36:47 -0500

    conditionally render api url for development or production

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,12 @@
-const apiUrl = 'https://fitness-buddy-hwm.cyclic.app/api';
+let apiUrl;
+
+if (
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1'
+) {
+  apiUrl = 'http://localhost:3000/api';
+} else {
+  apiUrl = 'https://fitness-buddy-hwm.cyclic.app/api';
+}
 
 export default apiUrl;
